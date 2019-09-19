c2366d4ef896232b3299163a907ad9c5bb93ff67
Paul Baclace <peb@pebmac4.localdomain>
2019-09-19 14:40:37 -0700

    add BERT env vars, use relative path

--- start_api_service.sh
+++ start_api_service.sh
@@ -2,8 +2,10 @@
 eval "$(conda shell.bash hook)"
 conda activate research-pub
 
-export FT_MODEL=/home/peb/ws/fastText/dataset20000.bin
-export FT_URL_MODEL=/home/peb/ws/fastText/url_test_20000.bin
+export FT_MODEL=../fastText/dataset20000.bin
+export FT_URL_MODEL=../fastText/url_test_20000.bin
+export TF_IMAGE_SERVER_HOSTPORT=localhost:8501
+export TF_BERT_VOCAB_PATH=../bert_model_base_multilingual/multi_cased_L-12_H-768_A-12/vocab.txt
 export TF_IMAGE_SERVER_HOSTPORT=localhost:8501
 
 if [ ! -e ./app.py ]; then
