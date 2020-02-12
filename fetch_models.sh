106adc7a1aaac6b55929c57d6dfc1fe4de9b4b91
Bryan Newbold <bnewbold@archive.org>
2020-02-11 18:47:07 -0800

    fetch_models: be less verbose

--- fetch_models.sh
+++ fetch_models.sh
@@ -2,4 +2,4 @@
 
 # fetch model files!
 mkdir -p model_snapshots
-wget --recursive --no-clobber --no-parent --no-host-directories --cut-dirs=2 --reject "index.html*" --reject "robots.txt" --directory-prefix model_snapshots "https://archive.org/download/pdf_trio_models/"
+wget --no-verbose --recursive --no-clobber --no-parent --no-host-directories --cut-dirs=2 --reject "index.html*" --reject "robots.txt" --directory-prefix model_snapshots "https://archive.org/download/pdf_trio_models/"
